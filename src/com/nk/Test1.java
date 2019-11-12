46b8f8b2ce4032dfd1d6eb7b38fcc81c47288848
sonu <sonu@192.168.0.13>
2019-11-12 22:45:24 +0000

    first commit...

---
+++ src/com/nk/Test1.java
@@ -0,0 +1,14 @@
+package com.nk;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String msg = "Hello";
+		if (msg.equalsIgnoreCase("hi")) {
+			System.out.println("hi world");
+		} else {
+			System.out.println(msg + " world!!!");
+		}
+	}
+
+}
